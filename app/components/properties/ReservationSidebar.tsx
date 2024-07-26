6b8ae95cef8f24f22148f898dec275d0fdbbe562
Dhruv Sharma <dhruv999ddrr@gmail.com>
2024-07-26 04:38:00 -0400

    made reservation ui and calculations dynamic and added calendar component to select the booking dates

--- app/components/properties/ReservationSidebar.tsx
+++ app/components/properties/ReservationSidebar.tsx
@@ -1,29 +1,129 @@
+'use client';
+
+import { useState, useEffect } from "react";
+import { Calendar, Range } from 'react-date-range';
+import { differenceInDays, eachDayOfInterval } from "date-fns";
+
+import apiService from "@/app/services/apiService";
+import useLoginModal from "@/app/hooks/useLoginModal";
+import DatePicker from "../forms/Calendar";
+
+const initialDateRange = {
+    startDate: new Date(),
+    endDate: new Date(),
+    key: 'selection',
+
+}
+
 export type Property = {
     id: string;
     price_per_night: number;
+    guests: number;
 
 }
 
 interface ReservationSidebarProps {
+    userId: string | null,
     property: Property
 }
 
 const ReservationSidebar: React.FC<ReservationSidebarProps> = ({
-    property
+    property,
+    userId
 }) => {
+
+    const loginModal = useLoginModal();
+
+    const [fee, setFee] = useState<number>(0)
+    const [nights, setNights] = useState<number>(1)
+    const [totalPrice, setTotalPrice] = useState<number>(0)
+    const [dateRange, setDateRange] = useState<Range>(initialDateRange)
+    const [minDate, setMinDate] = useState<Date>(new Date())
+    const [guests, setGuests] = useState<string>('1')
+
+    const guestsRange = Array.from({ length: property.guests }, (_, index) => index + 1)
+
+    const _setDateRange = (selection: any) => {
+        const newStartDate = new Date(selection.startDate)
+        const newEndDate = new Date(selection.endDate)
+
+        if (newEndDate <= newStartDate) {
+            console.log("correcting dates ..")
+            newEndDate.setDate(newStartDate.getDate() + 1)
+
+        }
+
+        console.log("new : ", newStartDate, " , ", newEndDate)
+
+        setDateRange({
+            ...dateRange,
+            startDate: newStartDate,
+            endDate: newEndDate,
+
+        })
+    }
+
+    useEffect(() => {
+        if (dateRange.startDate && dateRange.endDate) {
+            const dayCount = differenceInDays(
+                dateRange.endDate,
+                dateRange.startDate
+            )
+
+            if (dayCount && property.price_per_night) {
+                const calculated_fee = (dayCount * property.price_per_night) * 0.05
+                setFee(calculated_fee)
+                setTotalPrice((dayCount * property.price_per_night) + calculated_fee)
+                setNights(dayCount)
+            }
+            else {
+                // default calculation is wrt 1
+                const calculated_fee = (property.price_per_night) * 0.05
+                setFee(calculated_fee)
+                setTotalPrice(property.price_per_night + calculated_fee)
+                setNights(1)
+            }
+        }
+    }, [dateRange])
+
     return (
         <aside className="mt-6 p-6 col-span-2 rounded-xl border border-gray-300 shadow-xl">
-            This is the reservation sidebar now ..
+
             <h2 className="mb-5 text-2xl ">${property.price_per_night} per night</h2>
+
+            <DatePicker
+                value={dateRange}
+                onChange={(value) => _setDateRange(value.selection)}
+            >
+
+            </DatePicker>
+
             <div className="mb-6 p-3 border border-gray-400 rounded-xl">
                 <label className="mb-2 block font-bold text-xs">Guests</label>
-                <select className="w-full -ml-1 text-xm">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
+
+
+                <select
+                    value={guests}
+                    onChange={(e) => setGuests(e.target.value)}
+                    className="w-full -ml-1 text-xm"
+                >
+                    {
+                        guestsRange.map((number) => {
+                            return (
+                                <option
+                                    key={number}
+                                    value={number}
+                                >
+                                    {number}
+                                </option>
+                            )
+                        })
+                    }
+
+
                 </select>
+
+
             </div>
 
             <div className="w-full mb-6 py-6 text-center text-white bg-airbnb hover:bg-airbnb-dark rounded-xl">
@@ -32,16 +132,16 @@ const ReservationSidebar: React.FC<ReservationSidebarProps> = ({
 
             <div className="mb-4 flex justify-between align-center ">
                 <p>
-                    $200 * 4 nights
+                    ${property.price_per_night} * {nights} nights
                 </p>
-                <p>$800</p>
+                <p>${property.price_per_night * nights}</p>
             </div>
 
             <div className="mb-4 flex justify-between align-center ">
                 <p>
                     DhruvBnb fee
                 </p>
-                <p>$40</p>
+                <p>${fee}</p>
             </div>
 
             <hr />
@@ -50,7 +150,7 @@ const ReservationSidebar: React.FC<ReservationSidebarProps> = ({
                 <p>
                     Total
                 </p>
-                <p>$840</p>
+                <p>${totalPrice}</p>
             </div>
 
         </aside>
